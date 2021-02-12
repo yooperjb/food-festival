3356fc3e1d7fd14c181b9c8b6d3fde6d5a74c182
Pedal <jmb181@humboldt.edu>
2021-02-11 23:18:05 -0800

    manifest apparently created but still getting error and no carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 require("bootstrap");
-//const img1 = require("../../assets/img/food-table.jpg");
-//const img2 = require("../../assets/img/grill.jpg");
-const img1 = require("../img/food-table.jpg");
-const img2 = require("../img/grill.jpg");
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
 
 $(document).ready(function() {
  
